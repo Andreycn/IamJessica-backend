5ab6455761ffd03182463f589a54cee747bf4db1
Andrey Nogueira <57120765+Andreycn@users.noreply.github.com>
2025-04-09 02:24:47 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,29 +10,29 @@ L = instaloader.Instaloader()
 def scrape_profile():
     data = request.get_json()
     url = data.get('url')
-    if not url or \"instagram.com\" not in url:
-        return jsonify({\"error\": \"URL inválida.\"}), 400
+    if not url or "instagram.com" not in url:
+        return jsonify({"error": "URL inválida."}), 400
     try:
         username = url.rstrip('/').split('/')[-1]
         profile = instaloader.Profile.from_username(L.context, username)
         posts = []
         for post in profile.get_posts():
             posts.append({
-                \"caption\": post.caption[:200],
-                \"likes\": post.likes,
-                \"comments\": post.comments,
-                \"url\": post.url
+                "caption": post.caption[:200],
+                "likes": post.likes,
+                "comments": post.comments,
+                "url": post.url
             })
             if len(posts) >= 3:
                 break
         return jsonify({
-            \"username\": profile.username,
-            \"followers\": profile.followers,
-            \"posts\": profile.mediacount,
-            \"topPosts\": posts
+            "username": profile.username,
+            "followers": profile.followers,
+            "posts": profile.mediacount,
+            "topPosts": posts
         })
     except Exception as e:
-        return jsonify({\"error\": str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
